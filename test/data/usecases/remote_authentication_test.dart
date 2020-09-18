b3ebf4a95f407c65d1826fad1f34a7e00e88d263
Lucas Luz <lucasluzthatsme@gmail.com>
2020-09-18 10:16:57 -0300

    ensure RemoteAuthentication to calls HttpClient with corret body values

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,22 +1,26 @@
+import 'package:ForDev/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class RemoteAuthentication{
+class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
-
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
-  Future<void> auth() async {
-    await httpClient.request(url: url,method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    await httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret});
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url,@required String method});
+  Future<void> request(
+      {@required String url, @required String method, Map body});
 }
 
 class MockHttpClient extends Mock implements HttpClient {}
@@ -25,15 +29,21 @@ void main() {
   RemoteAuthentication sut;
   HttpClient httpClient;
   String url;
-  
-  setUp(() {
-     httpClient = MockHttpClient();
-     url = faker.internet.httpUrl();
-     sut = RemoteAuthentication(httpClient: httpClient, url: url);
-  });
-  test('Should call HttpClient with correct values', () async {
-    await sut.auth();
 
-    verify(httpClient.request(url: url,method:'post'));
+  setUp(() {
+    httpClient = MockHttpClient();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
+
+  
+  test('Should call HttpClient with correct values', () async {
+    final params = AuthenticationParams(email: faker.internet.email(),secret: faker.internet.password());
+    await sut.auth(params);
+
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret}));
   });
 }
