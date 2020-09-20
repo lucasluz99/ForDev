247712c5a77834e3c2f038bdec956483e7afa2e5
Lucas Luz <lucasluzthatsme@gmail.com>
2020-09-20 18:06:25 -0300

    ensure HttpAdapter throws ServerError on 500

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+
 import 'package:meta/meta.dart';
 import 'package:http/http.dart';
 
@@ -23,10 +24,12 @@ class HttpAdapter implements HttpClient {
   Map _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else if(response.statusCode == 204) {
+    } else if (response.statusCode == 204) {
       return null;
-    } else {
+    } else if (response.statusCode == 400) {
       throw HttpError.badRequest;
+    } else {
+      throw HttpError.serverError;
     }
   }
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -92,5 +92,14 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+
+     test('Should return BadRequestError if post returns 500', () async {
+      mockResponse(500);
+
+     final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
