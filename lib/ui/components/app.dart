b16259c904659972cc6b12374ea73cd3df0ecc4d
Lucas Luz <lucasluzthatsme@gmail.com>
2020-09-21 07:00:58 -0300

    Add LoginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -5,10 +5,45 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
     return MaterialApp(
-      title: 'ForDev',
-      debugShowCheckedModeBanner: false,
-      home: LoginPage()
-    );
+        title: 'ForDev',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: primaryColor,
+            primaryColorDark: primaryColorDark,
+            primaryColorLight: primaryColorLight,
+            accentColor: primaryColor,
+            backgroundColor: Colors.white,
+
+            textTheme: TextTheme(
+                headline1: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColorDark)),
+
+            inputDecorationTheme: InputDecorationTheme(
+              alignLabelWithHint: true,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColorLight),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: primaryColor))),
+                    
+            
+            buttonTheme: ButtonThemeData(
+              colorScheme: ColorScheme.light(primary:primaryColor),
+              buttonColor: primaryColor,
+              splashColor: primaryColorLight,
+              padding: const EdgeInsets.symmetric(vertical:10,horizontal: 20),
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20))
+            )
+            ),
+
+            
+        home: LoginPage());
   }
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,39 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
-            ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        icon: Icon(Icons.email), labelText: 'Email'),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        icon: Icon(Icons.lock), labelText: 'Senha'),
-                  ),
-                  RaisedButton(
-                    child: Text('Entrar'.toUpperCase()),
-                    onPressed: () {},
-                  ),
-                  FlatButton.icon(
-                    icon: Icon(Icons.person),
-                    label: Text('Criar conta'),
-                    onPressed: () {},
-                  )
-                ],
+            LoginHeader(),
+            Headline1(text:'Login'),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                          labelText: 'Email'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32, top: 10),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.lock,
+                                color: Theme.of(context).primaryColorLight),
+                            labelText: 'Senha'),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('Entrar'.toUpperCase()),
+                      onPressed: () {},
+                    ),
+                    FlatButton.icon(
+                      icon: Icon(Icons.person),
+                      label: Text('Criar conta'),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
               ),
             )
           ],
