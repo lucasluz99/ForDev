5f11a421a16607abffd97d1c6cbe1ed6fce870df
Lucas Luz <lucasluzthatsme@gmail.com>
2020-09-25 14:57:45 -0300

    Move StreamLoginPresenter to its own file

--- test/presentation/stream_login_presenter_test.dart
+++ test/presentation/stream_login_presenter_test.dart
@@ -1,36 +1,11 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class LoginState {
-  String emailError;
-}
-
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
+import 'package:ForDev/presentation/presenters/stream_login_presenter.dart';
+import 'package:ForDev/presentation/protocols/validation.dart';
 
 class MockValidation extends Mock implements Validation {}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
 
 void main() {
   Validation validation;
@@ -62,6 +37,7 @@ void main() {
 
   test('Should emit email error if validation fails', () {
     mockValidation(value: 'error');
+
     expectLater(sut.emailErrorStream, emits('error'));
 
     sut.validateEmail(email);
