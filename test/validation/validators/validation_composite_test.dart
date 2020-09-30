81932d65b61fc228ad1643dacad8d0fdcdd898d6
Lucas Luz <lucasluzthatsme@gmail.com>
2020-09-30 10:23:19 -0300

    ensure ValidationComposite returns the first error of field

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -11,11 +11,17 @@ class ValidationComposite implements Validation {
 
   String validate({@required String field, @required String value}) {
     String error;
-    for(final validation in validations){
-       error = validation.validate(value);
-       if(error?.isNotEmpty == true) {
-         return error;
-       }
+    final filterValidations =
+        validations.where((e) => e.field == field).toList();
+
+    for (final validation in filterValidations) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    if (error == '') {
+      return null;
     }
     return error;
   }
@@ -37,13 +43,13 @@ void main() {
     when(validation2.validate(any)).thenReturn(error);
   }
 
-   void mockValidation3(String error) {
+  void mockValidation3(String error) {
     when(validation3.validate(any)).thenReturn(error);
   }
 
   setUp(() {
     validation1 = MockFieldValidation();
-    when(validation1.field).thenReturn('any');
+    when(validation1.field).thenReturn('other');
     mockValidation1(null);
 
     validation2 = MockFieldValidation();
@@ -51,26 +57,25 @@ void main() {
     mockValidation2(null);
 
     validation3 = MockFieldValidation();
-    when(validation3.field).thenReturn('other');
+    when(validation3.field).thenReturn('any');
     mockValidation3(null);
 
-    sut = ValidationComposite([validation1, validation2,validation3]);
+    sut = ValidationComposite([validation1, validation2, validation3]);
   });
   test('Should return null if all validations returns null or empty', () {
     mockValidation2('');
 
     final error = sut.validate(field: 'any', value: 'any');
-
     expect(error, null);
   });
 
-   test('Should return the first error', () {
+  test('Should return the first error', () {
     mockValidation1('error_1');
     mockValidation2('error_2');
     mockValidation3('error_3');
-    
+
     final error = sut.validate(field: 'any', value: 'any');
 
-    expect(error, 'error_1');
+    expect(error, 'error_2');
   });
 }
