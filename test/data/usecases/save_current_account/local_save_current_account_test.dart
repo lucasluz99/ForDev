c69cac8f58071312d343d71d372120934ffe84f4
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-02 15:51:00 -0300

    move production code to its own files

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,30 +1,12 @@
-import 'package:ForDev/domain/helpers/helpers.dart';
 import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:ForDev/domain/helpers/helpers.dart';
 import 'package:ForDev/domain/entities/entities.dart';
-import 'package:ForDev/domain/usecases/save_current_account.dart';
 
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({this.saveSecureCacheStorage});
-
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-          key: 'token', value: account.token);
-    } catch (e) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({@required String key, @required String value});
-}
+import 'package:ForDev/data/cache/cache.dart';
+import 'package:ForDev/data/usecases/usecases.dart';
 
 class MockSaveSecureCacheStorage extends Mock
     implements SaveSecureCacheStorage {}
@@ -50,7 +32,7 @@ void main() {
   test('Should throw UnexpectedError if SaveSecureCacheStorage throws',
       () async {
     when(saveSecureCacheStorage.saveSecure(
-        key: anyNamed('key'), value: anyNamed('value')));
+        key: anyNamed('key'), value: anyNamed('value'))).thenThrow(DomainError.unexpected);
     final future = sut.save(account);
 
     expect(future, throwsA(DomainError.unexpected));
