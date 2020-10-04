59e2e3fd537a316e5be11eb220026fe1e126f465
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-04 18:03:04 -0300

    move LocalStorageAdapter to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,20 +1,10 @@
-import 'package:ForDev/data/cache/save_secure_cache_storage.dart';
-import 'package:ForDev/domain/helpers/helpers.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
 
-  LocalStorageAdapter({@required this.secureStorage});
 
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
+import 'package:ForDev/infra/cache/cache.dart';
 
 class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
 
@@ -36,5 +26,11 @@ void main() {
     verify(secureStorage.write(key: key, value: value));
   });
 
-  
+  test('Should throw if saveSecure throws', () async {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
+  });
 }
