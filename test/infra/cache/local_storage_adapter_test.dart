3da029c6bf72e68f8d25404dcaf86605f4be94d0
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-04 17:55:15 -0300

    create setUp method

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -10,35 +10,31 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
 
   LocalStorageAdapter({@required this.secureStorage});
 
-   Future<void> saveSecure({@required String key, @required String value}) async {
-     await secureStorage.write(key: key, value: value);
-   }
+  Future<void> saveSecure(
+      {@required String key, @required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
 }
 
 class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
-void main() {
-  test('Should call saveSecure with correct values', () async {
-    final secureStorage = MockFlutterSecureStorage();
-    final sut = LocalStorageAdapter(secureStorage:secureStorage);
-    String key = 'token';
-    String value = 'value';
 
+void main() {
+  FlutterSecureStorage secureStorage = MockFlutterSecureStorage();
+  LocalStorageAdapter sut = LocalStorageAdapter(secureStorage: secureStorage);
+  String key = 'token';
+  String value = 'value';
+
+  setUp(() {
+    secureStorage = MockFlutterSecureStorage();
+    sut = LocalStorageAdapter(secureStorage: secureStorage);
+    key = 'token';
+    value = 'value';
+  });
+  test('Should call saveSecure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
   });
 
-  test('Should call saveSecure with correct values', () async {
-    final secureStorage = MockFlutterSecureStorage();
-    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value'))).thenThrow(DomainError.unexpected);
-    final sut = LocalStorageAdapter(secureStorage:secureStorage);
-    String key = 'token';
-    String value = 'value';
-
-    final future = sut.saveSecure(key: key, value: value);
-
-   expect(future,throwsA(DomainError.unexpected));
-  });
-
-
+  
 }
