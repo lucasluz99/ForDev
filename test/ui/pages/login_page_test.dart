6b5de2b53b4930e3b483b6020969a7a1fa4e9ddc
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-07 10:43:17 -0300

    move SplashPage to its own file

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ForDev/ui/pages/pages.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:ForDev/ui/pages/pages.dart';
 
 class MockLoginPresenter extends Mock implements LoginPresenter {}
 
