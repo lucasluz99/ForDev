b5e225805ee11ffeef11b26b441cf95880960cae
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-07 10:09:46 -0300

    ensure SplashPage calls loadCurrentAccount

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
 
 class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({@required this.presenter});
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
     return Scaffold(
         appBar: AppBar(
           title: Text('4Dev'),
@@ -15,15 +20,25 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class MockSplashPresenter extends Mock implements SplashPresenter {}
+
 void main() {
+  SplashPresenter presenter;
   Future<void> loadPage(WidgetTester tester) async {
-    await tester.pumpWidget(GetMaterialApp(
-        initialRoute: '/',
-        getPages: [GetPage(name: '/', page: () => SplashPage())]));
+    presenter = MockSplashPresenter();
+    await tester.pumpWidget(GetMaterialApp(initialRoute: '/', getPages: [
+      GetPage(name: '/', page: () => SplashPage(presenter: presenter))
+    ]));
   }
 
-  testWidgets('Should present a spinner', (WidgetTester tester) async {
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
     await loadPage(tester);
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
