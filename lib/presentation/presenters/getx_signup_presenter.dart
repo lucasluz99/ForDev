01c329f7a0a7d126253d0d96d29e8c051515b182
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-12 15:43:19 -0300

    ensure GetxSignUpPresenter valid email

---
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -0,0 +1,49 @@
+import 'package:get/get.dart';
+
+import '../../domain/helpers/domain_error.dart';
+import '../../ui/helpers/errors/errors.dart';
+
+import '../../ui/pages/login/login_presenter.dart';
+
+import '../../domain/usecases/usecases.dart';
+
+import '../protocols/validation.dart';
+
+class GetxSignUpPresenter extends GetxController {
+  final Validation validation;
+
+  var _emailError = Rx<UiError>();
+
+  var _isFormValid = false.obs;
+
+  Stream<UiError> get emailErrorStream => _emailError.stream;
+
+  Stream<bool> get isFormValidStream => _isFormValid.stream;
+
+  GetxSignUpPresenter({
+    this.validation,
+  });
+
+  void validateEmail(String email) {
+    _emailError.value = _validateField(field: 'email', value: email);
+    _validateForm();
+  }
+
+  void _validateForm() {
+    _isFormValid.value = false;
+  }
+
+  UiError _validateField({String field, String value}) {
+    final error = validation.validate(field: field, value: value);
+    switch (error) {
+      case ValidationError.invalidField:
+        return UiError.invalidField;
+      case ValidationError.requiredField:
+        return UiError.requiredField;
+      default:
+        return null;
+    }
+  }
+
+  void dispose() {}
+}
---
+++ test/presentation/getx_signup_presenter_test.dart
@@ -0,0 +1,106 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:ForDev/domain/usecases/usecases.dart';
+import 'package:ForDev/domain/helpers/domain_error.dart';
+import 'package:ForDev/domain/entities/entities.dart';
+import 'package:ForDev/domain/usecases/authentication.dart';
+
+import 'package:ForDev/ui/helpers/errors/errors.dart';
+
+import 'package:ForDev/presentation/presenters/presenters.dart';
+import 'package:ForDev/presentation/protocols/validation.dart';
+
+class MockValidation extends Mock implements Validation {}
+
+class MockSaveCurrentAccount extends Mock implements SaveCurrentAccount {}
+
+void main() {
+  GetxSignUpPresenter sut;
+  Validation validation;
+  SaveCurrentAccount saveCurrentAccount;
+  Authentication authentication;
+  String email;
+  String password;
+  String token;
+
+  PostExpectation mockValidatationCall(String field) {
+    return when(validation.validate(
+        field: field == null ? anyNamed('field') : field,
+        value: anyNamed('value')));
+  }
+
+  void mockValidation({String field, ValidationError value}) {
+    mockValidatationCall(field).thenReturn(value);
+  }
+
+  PostExpectation mockAuthenticationCall() {
+    return when(authentication.auth(any));
+  }
+
+  PostExpectation mockSaveCurrentAccountCall() {
+    return when(saveCurrentAccount.save(any));
+  }
+
+  void mockAuthentication() {
+    mockAuthenticationCall().thenAnswer((_) async => AccountEntity(token));
+  }
+
+  void mockAuthenticationError(DomainError error) {
+    mockAuthenticationCall().thenThrow(error);
+  }
+
+  void mockSaveCurrentAccountError() {
+    mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
+  }
+
+  setUp(() {
+    validation = MockValidation();
+    saveCurrentAccount = MockSaveCurrentAccount();
+    sut = GetxSignUpPresenter(validation: validation);
+
+    email = faker.internet.email();
+    password = faker.internet.password();
+    token = faker.guid.guid();
+  });
+
+  test('Should call Validation with correct email', () {
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit invalidFieldError if email is invalid', () {
+    mockValidation(value: ValidationError.invalidField);
+
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, UiError.invalidField)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should emit requiredFieldError if email is empty', () {
+    mockValidation(value: ValidationError.requiredField);
+
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, UiError.requiredField)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should emit null if emails validation succeeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+}
