325de48af2887d7f62b24e79d05f6441128c1898
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-14 11:32:14 -0300

    make RemoteLoadSurveys implements LoadSurveys

--- lib/data/usecases/load_surveys/remote_load_surveys.dart
+++ lib/data/usecases/load_surveys/remote_load_surveys.dart
@@ -1,11 +1,12 @@
 import 'package:meta/meta.dart';
 
+import '../../../domain/usecases/usecases.dart';
 import '../../../domain/helpers/helpers.dart';
 import '../../../domain/entities/entities.dart';
 import '../../models/models.dart';
 import '../../http/http.dart';
 
-class RemoteLoadSurveys {
+class RemoteLoadSurveys implements LoadSurveys {
   final HttpClient<List<Map>> httpClient;
   final String url;
 
