9dd322aecbdd7b509f898072abb2cc265ca25133
Lucas Luz <lucasluzthatsme@gmail.com>
2020-10-14 14:17:49 -0300

    ensure HttpAdapter throws ServerError on get fails

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -250,5 +250,13 @@ void main() {
 
       expect(future, throwsA(HttpError.serverError));
     });
+  
+     test('Should return ServerError if get throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
